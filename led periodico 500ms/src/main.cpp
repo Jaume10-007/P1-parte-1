d7b055743e5d7d493afc7af9d475611c10a80785
Jaume Gracià Moya <jaume.gracia.moya@estudiantat.upc.edu>
2026-03-07 15:44:44 +0100

    Led parpadeo periódico 500ms

--- led periodico 500ms/src/main.cpp
+++ led periodico 500ms/src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// GPIO donde está conectado el LED
+// En muchas ESP32 el LED integrado suele estar en el pin 2
+#define LED_PIN 2
+
+// Tiempo en milisegundos (500 ms = 0.5 s)
+#define T 500
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // Configuramos el pin del LED como salida
+    pinMode(LED_PIN, OUTPUT);
+
+    // Inicializamos la comunicación serie a 115200 baudios
+    Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // Encendemos el LED (nivel alto = 3.3V)
+    digitalWrite(LED_PIN, HIGH);
+
+    // Enviamos mensaje por el puerto serie
+    Serial.println("ON");
+
+    // Esperamos 500 ms
+    delay(T);
+
+    // Apagamos el LED (nivel bajo = 0V)
+    digitalWrite(LED_PIN, LOW);
+
+    // Enviamos mensaje por el puerto serie
+    Serial.println("OFF");
+
+    // Esperamos otros 500 ms
+    delay(T);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
+
